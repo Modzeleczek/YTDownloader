c0064352c394d1d38e691dbf285da26581bcafef
Modzeleczek <lukasz.modzelewski1@gmail.com>
2020-09-24 02:14:48 +0200

    Add generation of download buttons in WebView using a converter API.

--- YTDownloader/YTDownloader.Windows/MainPage.xaml.cs
+++ YTDownloader/YTDownloader.Windows/MainPage.xaml.cs
@@ -27,14 +27,63 @@ namespace YTDownloader
             this.InitializeComponent();
         }
 
-        private void ConvertButton_Click(object sender, RoutedEventArgs e)
+        private string ExtractVideoID(string videoURL)
         {
-            
+            // https://www.youtube.com/watch?v=x16aMVCbmeg  standard URL
+            // youtu.be/x16aMVCbmeg shortened URL
+
+            string id;
+            // check if the URL is a standard YouTube URL
+            string soughtText = "youtube.com/watch?v=";
+            int index = videoURL.LastIndexOf(soughtText);
+            if (index != -1)
+            {
+                // get all characters after '='
+                id = videoURL.Substring(index + soughtText.Length);
+                // get the position of the first ampersand if any exists
+                index = id.IndexOf("&");
+                // get all characters from start to the ampersand if it exists
+                if (index != -1)
+                    id = id.Substring(0, index);
+            }
+            // otherwise, check if the URL is a shortened YouTube URL
+            else
+            {
+                soughtText = "youtu.be/";
+                index = videoURL.LastIndexOf(soughtText);
+                if (index != -1)
+                    id = videoURL.Substring(index + soughtText.Length);
+                // otherwise, the URL is not a recognizable YouTube video URL
+                else
+                    return null;
+            }
+            return id;
         }
 
-        private void Browser_NavigationCompleted(WebView sender, WebViewNavigationCompletedEventArgs args)
+        private void ConvertButton_Click(object sender, RoutedEventArgs e)
         {
+            string id = ExtractVideoID(URLTextBox.Text);
+            // if ID was not extracted, print an error message and stop this method
+            if (id == null)
+            {
+                FeedbackTextBox.Text = "Invalid YouTube URL.\r\n";
+                return;
+            }
+            // generate download buttons in WebView by navigating to API URI (https://github.com/matthew-asuncion/Fast-YouTube-to-MP3-Converter-API)
+            Browser.Navigate(new Uri("https://www.yt-download.org/api/button/mp3/" + id));
+            // https://www.yt-download.org/api/button/videos/
+        }
 
+        private async void Browser_NavigationCompleted(WebView sender, WebViewNavigationCompletedEventArgs args)
+        {
+            if (args.IsSuccess)
+                // print a success message
+                FeedbackTextBox.Text = "Navigation successful.\r\n";
+            else
+                // print an error message
+                FeedbackTextBox.Text = $"Navigation failed.\r\n{args.Uri}\r\n{args.WebErrorStatus}\r\n";
+
+            FeedbackTextBox.Text += await Browser.InvokeScriptAsync("eval", new string[] { "document.documentElement.outerHTML;" }) + "\r\n";
         }
     }
 }
