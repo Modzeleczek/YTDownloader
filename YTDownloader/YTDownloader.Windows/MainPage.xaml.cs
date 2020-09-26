00743ece3c97ccd618d3cfe08e08f35669e92d5f
Modzeleczek <lukasz.modzelewski1@gmail.com>
2020-09-27 01:19:40 +0200

    Extract methods from MainPage to new classes: Downloader and MP3. Add custom exceptions.

--- YTDownloader/YTDownloader.Windows/MainPage.xaml.cs
+++ YTDownloader/YTDownloader.Windows/MainPage.xaml.cs
@@ -15,6 +15,7 @@ using Windows.UI.Xaml.Navigation;
 using System.Net;
 using System.Threading.Tasks;
 using Windows.Storage;
+using YTDownloader.Exceptions;
 
 // The Blank Page item template is documented at http://go.microsoft.com/fwlink/?LinkId=234238
 
@@ -25,15 +26,73 @@ namespace YTDownloader
     /// </summary>
     public sealed partial class MainPage : Page
     {
+        private Downloader DownloaderInstance = new Downloader();
         public MainPage()
         {
             this.InitializeComponent();
         }
+        private async void ConvertButton_Click(object sender, RoutedEventArgs e)
+        {
+            try
+            {
+                var mp3s = await DownloaderInstance.GetMP3sAsync(URLTextBox.Text);
+                FeedbackTextBox.Text = "";
+                foreach (var mp3 in mp3s)
+                    FeedbackTextBox.Text += $"{mp3}\r\n";
+                
+                // TODO: users can choose, which bitrate they want to download
+            }
+            catch (Exception ex) when (
+            ex is DownloaderAPIException ||
+            ex is ArgumentException ||
+            ex is UnknownNameException ||
+            ex is IOException)
+            { FeedbackTextBox.Text += ex.ToString(); }
+        }
+    }
+
+    public class Downloader
+    {
+        /// <summary>
+        /// Returns an array of read-only MP3 objects.
+        /// </summary>
+        /// <param name="videoUrl">URL of the YouTube video, whose sound should be downloaded.</param>
+        /// <returns>Array of read-only MP3 objects.</returns>
+        public async Task<MP3[]> GetMP3sAsync(string videoUrl)
+        {
+            string id = ExtractVideoID(videoUrl);
+            // an ArgumentException is thrown if ID was not extracted
+
+            // download source of the webpage generated for specified video by the API
+            // https://github.com/matthew-asuncion/Fast-YouTube-to-MP3-Converter-API
+            string source = await DownloadSourceAsync("https://www.yt-download.org/api/button/mp3/" + id);
+            // an System.UriFormatException is thrown if the URL is invalid
+            // to download videos instead of MP3s: "https://www.yt-download.org/api/button/videos/" + id
+
+            // find download URLs
+            var urls = source.SubstringsBeginningAndEnding("https://www.yt-download.org/download/", '"');
+            if (urls.Count == 0)
+                throw new DownloaderAPIException("No download URLs were generated.");
+
+            // construct an array containing videos
+            var videos = new List<MP3>();
+            // example download URL: https://www.yt-download.org/download/C4dbd_dynKs/mp3/320/1601086910/ee4995636533712c10fad8d28a95b4966cb17d3e0a08f0e38da6905a86076ef1/0
+            for (int i = 0; i < urls.Count; ++i)
+            {
+                var urlParts = urls[i].Split('/');
+                if (urlParts.Length >= 7)
+                {
+                    videos.Add(new MP3(urls[i], int.Parse(urlParts[6])));
+                    // a FormatException is thrown if the bitrate part is not a valid integer
+                }
+            }
+            return videos.ToArray();
+        }
 
         private string ExtractVideoID(string videoURL)
         {
-            // https://www.youtube.com/watch?v=x16aMVCbmeg  standard URL
-            // youtu.be/x16aMVCbmeg shortened URL
+            // standard URL example: https://www.youtube.com/watch?v=x16aMVCbmeg
+            // short URL example: youtu.be/x16aMVCbmeg
 
             string id;
             // check if the URL is a standard YouTube URL
@@ -58,103 +117,93 @@ namespace YTDownloader
                     id = videoURL.Substring(index + soughtText.Length);
                 // otherwise, the URL is not a recognizable YouTube video URL
                 else
-                    return null;
+                    throw new ArgumentException("Invalid YouTube URL.", "url");
             }
             return id;
         }
 
-        private async Task<string> DownloadSourceAsync(string URL)
+        private async Task<string> DownloadSourceAsync(string url)
         {
             // create a new HTTP web request
-            var http = (HttpWebRequest)WebRequest.Create(URL);
+            var http = (HttpWebRequest) WebRequest.Create(url);
             // download webpage's response and save a reference to it
-            var response = await http.GetResponseAsync();
+            using (var response = await http.GetResponseAsync())
             // save a reference to the data stream associated with the response
             using (var stream = response.GetResponseStream())
             // create a stream reader on response's stream
             using (var sr = new StreamReader(stream))
-            {
                 // read all response's bytes as string
                 return sr.ReadToEnd();
-            }
+        }
+    }
+
+    public class MP3
+    {
+        public string URL { get; }
+        public int Bitrate { get; }
+
+        public MP3(string url, int bitrate)
+        {
+            URL = url;
+            Bitrate = bitrate;
         }
 
-        private async void ConvertButton_Click(object sender, RoutedEventArgs e)
+        public override string ToString() => $"URL: {URL}; bitrate: {Bitrate}";
+
+        /// <summary>
+        /// Downloads and saves the MP3 in specified folder.
+        /// </summary>
+        /// <param name="folder">The folder, where the MP3 file will be saved.</param>
+        public async void DownloadAsync(StorageFolder folder)
         {
-            string id = ExtractVideoID(URLTextBox.Text);
-            // if ID was not extracted, print an error message and stop this method
-            if (id == null)
-            {
-                FeedbackTextBox.Text = "Invalid YouTube URL.\r\n";
-                return;
-            }
-            // download source of the webpage generated for specified video by the API
-            // https://github.com/matthew-asuncion/Fast-YouTube-to-MP3-Converter-API
-            string source = await DownloadSourceAsync("https://www.yt-download.org/api/button/mp3/" + id);
-            // https://www.yt-download.org/api/button/videos/
+            if (folder == null)
+                throw new ArgumentException("Folder cannot be null.", "folder");
 
-            // find download URLs
-            var urls = source.SubstringsBeginningAndEnding("https://www.yt-download.org/download/", '"');
-            if (urls.Count == 0)
-            {
-                FeedbackTextBox.Text = "No download URLs were generated.\r\n";
-                return;
-            }
-            // select the last generated URL (it is the one with 128 kbps MP3)
-            var url = urls[urls.Count - 1];
-
-            // download the selected MP3 from its URL
-            var http = (HttpWebRequest)WebRequest.Create(url);
+            // download the file from its URL
+            var http = (HttpWebRequest)WebRequest.Create(URL);
             var response = await http.GetResponseAsync();
-            // get the response's header, which contains the name of the MP3
-            // attachment; filename="Trance Allstars - Lost in Love (128 kbps).mp3"
+            // get the response's header, which contains the name of the file
+            // header example: attachment; filename="Trance Allstars - Lost in Love (128 kbps).mp3"
             string header = response.Headers["Content-Disposition"];
 
-            // retrieve the name of the MP3 from the header
-            const string soughtText = "filename=\"";
-            int beginning = header.IndexOf(soughtText);
-            if (beginning == -1)
-            {
-                FeedbackTextBox.Text += $"'{soughtText}' was not found in response's 'Content-Disposition' header: {header}.\r\n";
-                return;
-            }
-            beginning += soughtText.Length;
-            int end = header.IndexOf('"', beginning);
-            if (end == -1)
-            {
-                FeedbackTextBox.Text += $"No ending '\"' was found in response's 'Content-Disposition' header: {header}.\r\n";
-                return;
-            }
-            string fileName = header.Substring(beginning, end - beginning);
-            if(fileName.Length == 0)
-            {
-                FeedbackTextBox.Text += "File name stored in header is empty.\r\n";
-                return;
-            }
+            // retrieve the name of the file from the header
+            var substrings = header.SubstringsBeginningAndEnding("filename=\"", '"');
+            if (substrings.Count == 0)
+                throw new UnknownNameException("No MP3 file's name was found in response's " +
+                    $"'Content-Disposition' header: '{header}'.");
+            var fileName = substrings.FirstOrDefault();
 
-            // build the MP3 file from bytes copied from the response
+            // build the file from bytes copied from the response
             using (var responseStream = response.GetResponseStream())
             {
-                // get Music folder
-                var folder = KnownFolders.MusicLibrary;
-                if (folder == null)
-                {
-                    FeedbackTextBox.Text += "Music folder is inaccessible.\r\n";
-                    return;
-                }
-                // create an empty .mp3 file in the Music folder
+                // create an empty file in the folder
                 var file = await folder.CreateFileAsync(fileName, CreationCollisionOption.GenerateUniqueName);
                 if (file == null)
-                {
-                    FeedbackTextBox.Text += "Cannot create a file to save the downloaded MP3.\r\n";
-                    return;
-                }
-                // prepare a stream used to fill the .mp3 file
-                var fileWriteStream = await file.OpenStreamForWriteAsync();
-                // fill the file copying bytes from the response stream
-                responseStream.CopyTo(fileWriteStream);
-                FeedbackTextBox.Text += "Successfully downloaded MP3.\r\n";
+                    throw new IOException("Cannot create a file to save the downloaded bytes.");
+                // prepare a stream used to fill the created file
+                using (var fileWriteStream = await file.OpenStreamForWriteAsync())
+                    // fill the file copying bytes from the response stream
+                    responseStream.CopyTo(fileWriteStream);
             }
+        }
+    }
+
+    namespace Exceptions
+    {
+        public class UnknownNameException : Exception
+        {
+            public UnknownNameException() { }
+            public UnknownNameException(string message) : base(message) { }
+        }
+        public class IncompleteURLException : Exception
+        {
+            public IncompleteURLException() { }
+            public IncompleteURLException(string message) : base(message) { }
+        }
+        public class DownloaderAPIException : Exception
+        {
+            public DownloaderAPIException() { }
+            public DownloaderAPIException(string message) : base(message) { }
         }
     }
 
@@ -179,7 +228,7 @@ namespace YTDownloader
         /// <param name="str">String, where to search substrings.</param>
         /// <param name="beginning">String, which has to be at the beginning of a returned substring.</param>
         /// <param name="end">Character, which has to be the first character after a returned substring.</param>
-        /// <returns>List of substrings, which begin and end with specified strings.</returns>
+        /// <returns>List of substrings, which begin with specified string and end with specified character.</returns>
         public static List<string> SubstringsBeginningAndEnding(this string str, string beginning, char end)
         {
             if (String.IsNullOrEmpty(beginning))
